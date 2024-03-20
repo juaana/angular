3251537914b114cc8db501bfced5880a31bbae26
juaana <jjuanazabaleta@gmail.com>
2024-03-20 15:20:31 -0300

    Primera entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    NavbarComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
